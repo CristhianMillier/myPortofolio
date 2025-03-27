822683ff5e3251acf7d9bb6b523aceb490d2b2fe
Cristhian Millier <millierflores123@gmail.com>
2025-03-26 20:30:44 -0500

    first commit

---
+++ src/lib/data.ts
@@ -0,0 +1,148 @@
+export const personalInfo = {
+  name: "Cristhian Flores",
+  location: "Jaén, Cajamarca",
+  email: "millierpasapera123@gmail.com",
+  github: "https://github.com/CristhianMillier",
+  linkedin: "https://www.linkedin.com/in/cristhian-millier-flores-pasapera-206683281//",
+};
+
+export const workExperience = [
+  {
+    company: "Constructores & Consultores AP & RC S.A.C.",
+    location: "Trujillo, La Libertad",
+    position: "Product Manager",
+    period: "Jun 2024 - Feb 2025",
+    achievements: [
+      "Coordinación con Clientes (Operadores y Empresas de Telecomunicaciones).",
+      "Monitoreo remoto al personal en campo, utilizando aplicativo ISDP de Huawei.",
+      "Trabajar con el equipo técnico para resolver incidencias y mejorar procesos.",
+      "Coordinar la instalación, configuración y mantenimiento de equipos Huawei para MBB (banda ancha móvil), Datacon (conectividad de datos) y DWDM (multiplexación por división de longitud de onda densa).",
+      "Mantener actualizados los procedimientos internos según las mejores prácticas de Huawei.",
+      "Coordinar con los equipos de campo para asegurar que las instalaciones y configuraciones se realicen según los plazos establecidos.",
+    ],
+  },
+  {
+    company: "Fuerte Vencedores Del Cenepa de la Sexta Brigada de Selva",
+    location: "Bagua, Amazonas",
+    position: "Sección Telemática",
+    period: "Ene 2023 - Jun 2023",
+    achievements: [
+      "Desarrollo de aplicación Móvil para el área de SIB.",
+      "ODesarrollo de aplicación de Escritorio para el área de Mesa de Partes.",
+      "Mantenimiento de preventivo de PCs y Laptops.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Universidad Nacional Toribio Rodríguez de Mendoza",
+    location: "Bagua, Amazonas",
+    degree: "Bachiller Ingeniería de Sistemas",
+    period: "Mar 2019 - Dic 2023",
+    achievements: [
+      "Desarrollo de aplicaciones de escritorio usando JAVA y SQL Server",
+      "Desarrollo de aplicaciones web usando PHP, Laravel y MySQL",
+      "Gestión de proyuectos utilizando la metodología Ágil SCRUM",
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Java",
+    "PHP",
+  ],
+  frontendDevelopment: [
+    "Bootstrap",
+    "Tailwind CSS",
+  ],
+  backendDevelopment: ["Laravel", "Spring Boot"],
+  databaseAndStorage: ["PostgreSQL", "MySql", "SQL Server"],
+};
+
+export const projects = [
+  {
+    title: "Municipalidad el Milagro",
+    github: "https://github.com/CristhianMillier/milagroMunicipalidad",
+    demo: "https://negocioscrimo.com/",
+    description: [
+      "Aplicación Web para llevar el control del personal que se encuentra en planilla (pagos).",
+      "Desarrollada con el lenguaje de programación PHP utilizando framework Laravel 10 y base de datos MySQL.",
+      "Para poder ingresar a DEMO, ingrese las siguientes credenciales: 'user: admin@gmail.com' --- 'password: admin#2024'.",
+    ],
+  },
+  {
+    title: "Municipalidad Distrital de Copallin",
+    github: "https://github.com/CristhianMillier/saneamiento",
+    description: [
+      "Aplicación de escritorio para llevar control de los recibos de Recursos Directamente Rcaudados (RDR).",
+      "La aplicación genera automatica los recibos los 15 de cada mes.",
+      "La aplicación identifica los morosos a aquellas personas que están debiendo por más de 3 meses.",
+      "Desarrollada con el lenguaje de programación Java 8 y base de datos SQL Server",
+    ],
+  },
+  {
+    title: "Centro de Desarrollo Integral de la Familia - Bagua, Amazonas",
+    github: "https://github.com/CristhianMillier/CEDIF",
+    description: [
+      "Aplicación de escritorio para llevar control de los alimentos que cuenta CEDIF - Bagua.",
+      "Desarrollada con el lenguaje de programación Java 8 y base de datos SQL Server",
+    ],
+  },
+  {
+    title: "Fuerte Vencedores Del Cenepa de la Sexta Brigada de Selva - SIB",
+    github: "https://github.com/CristhianMillier/6aBrigSelvaSib",
+    description: [
+      "Aplicación móvil informativa.",
+    ],
+  },
+  {
+    title: "Fuerte Vencedores Del Cenepa de la Sexta Brigada de Selva - Mesa de Partes",
+    github: "https://github.com/CristhianMillier/6aBrigSelvaMesaParte",
+    description: [
+      "Aplicación de Escritorio para llevar el control de los documento rexeionados por el área de Mesa de Partes.",
+      "Desarrollada con el lenguaje de programación Java 8 y base de datos SQL Server",
+    ],
+  },
+  {
+    title: "Licorería MONARCA",
+    github: "https://github.com/CristhianMillier/monarca",
+    description: [
+      "Aplicación web para llevar el control de los productosm ventas y compra.",
+      "Desarrollada con el lenguaje de programación Java 8 y base de datos MySQL",
+    ],
+  },
+  {
+    title: "SABA",
+    github: "https://github.com/CristhianMillier/projectSaba",
+    description: [
+      "Aplicación móvil para llevar el control de la soya procesada.",
+      "Desarrollada con el lenguaje de programación Java 8 y base de datos SQL Server",
+    ],
+  },
+];
+
+export const awards = [
+  {
+    name: "Network equipment installation and configuration, commissioning, HSU and replacement",
+    issuer: "YOFC PERU S.A.C.",
+    date: "May 2024",
+  },
+  {
+    name: "Soporte Técnico de Computadoras",
+    issuer: "SKILL",
+    date: "Abr 2024",
+  },
+  {
+    name: "Desarrollo de Software de Facturación Electrónica con PHP",
+    issuer: "Centro de Entrenamiento en Técnlogías de Información",
+    date: "Abr 2024",
+  },
+  {
+    name: "Congreso Internacional de Tecnologías de la Información y la Comunicación - ICT",
+    issuer: "CONNECT UNI",
+    date: "Nov 2023",
+  },
+];
